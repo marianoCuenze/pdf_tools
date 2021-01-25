67c1c7c392ff2d0b9fb5750c733bf519340731ca
Mariano Cuenze <marianocuenze@gmail.com>
2021-01-24 22:49:09 -0300

    filters, test, and minor stuff

--- values/pdf_values.js
+++ values/pdf_values.js
@@ -8,55 +8,94 @@ H   hex    Hexa text
 M   nam    naMe
 A   arr    Array
 D   dic    Dict
-e   den    Dict Entry           <<aux type
+e   den    Dict Entry           val:    k = key name       v = key value
 S   stm    Stream               val:    i = Info in dic    d = raw Data in stream
 O   obj    Obj                  val:    i = obj Id         d = obj Data (value)
 R   Ref    obj Ref              val:    n = obj Num        g = obj Gen num
 * */
 export function pdf_val_null_new () {
     return {
-        "type": "pdf_val_null",
-        "val": null
+        "t": "N",
+        "v": null
     }
 }
 
-export function pdf_val_bool_new (val) {
+export function pdf_val_bool_new (v) {
 
     return {
-        "type": "pdf_val_bool",
-        "val": ( typeof val === 'string' ? val === "true" : val )
+        "t": "B",
+        "v": ( typeof v === 'string' ? v === "true" : v )
     }
 }
 
-export function pdf_val_num_new (val) {
+export function pdf_val_num_new (v) {
 
     return {
-        "type": "pdf_val_num",
-        "val": 1*val
+        "t": "U",
+        "v": 1*v
     }
 }
 
-export function pdf_val_str_txt_new (val) {
+export function pdf_val_str_txt_new (v) {
 
     return {
-        "type": "pdf_val_str_txt",
-        "val": val
+        "t": "T",
+        "v": v
     }
 }
 
-export function pdf_val_str_hex_new (val) {
+export function pdf_val_str_hex_new (v) {
 
     return {
-        "type": "pdf_val_hex_hex",
-        "val": val
+        "t": "H",
+        "v": v
     }
 }
 
 
-export function pdf_val_name_new (val) {
+export function pdf_val_name_new (v) {
 
     return {
-        "type": "pdf_val_name",
-        "val": val.substr(1)
+        "t": "M",
+        "v": v.substr(1)
+    }
+}
+
+
+export function pdf_val_arr_new (v) {
+
+    return {
+        "t": "A",
+        "v": v
+    }
+}
+
+export function pdf_val_dic_entry_new (k, v) {
+
+    return {
+        "t": "e",
+        "v": {  "k": k,
+                "v": v
+             }
+    }
+}
+
+export function pdf_val_stream_new (i, d) {
+
+    return {
+        "t": "S",
+        "v": { "i": i,
+               "d": d
+        }
+    }
+}
+
+export function pdf_val_obj_new (i, d) {
+
+    return {
+        "t": "O",
+        "v": { "i": i,
+               "d": d
+        }
     }
 }
